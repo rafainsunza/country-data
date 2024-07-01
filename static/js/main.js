dd49da00e515e9dff0b9dd3ce9bd1e9bbb606a5e
rafainsunza <rafainsunza@outlook.com>
2024-07-01 15:14:13 +0200

    Create dark-mode cookies instead of setting localstorage

--- static/js/main.js
+++ static/js/main.js
@@ -5,11 +5,40 @@ const body = document.querySelector('body');
 let maxPagesReached = false;
 let loadingData = false;
 let selectedCountry;
-const darkModeIsOn = localStorage.getItem('dark-mode') === 'enabled';
 
-localStorage.getItem('dark-mode') === null ?
-    localStorage.setItem('dark-mode', 'disabled') :
-    body.classList.toggle('dark-mode', localStorage.getItem('dark-mode') !== 'disabled');
+const setCookie = (cookieName, cookieValue, expirationDays) => {
+    const date = new Date();
+    date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000)); // Convert expiration days to miliseconds
+
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${cookieName}=${cookieValue};${expires};path=/`
+}
+
+const getCookies = () => {
+    const decodedCookies = decodeURIComponent(document.cookie);
+    const cookiesArray = decodedCookies.split(';').map(cookie => cookie.trim());
+    const cookieObjects = []
+
+    cookiesArray.forEach((cookie) => {
+        cookieObjects.push({
+            name: cookie.substring(0, cookie.indexOf('=')),
+            value: cookie.substring(cookie.indexOf('=') + 1, cookie[-1])
+        })
+    })
+
+    return cookieObjects
+}
+
+setInitialCookies = () => {
+    const cookies = getCookies();
+    const darkModeCookie = cookies.find((cookie) => cookie.name === 'dark-mode');
+
+    // If dark-mode cookie does not exist yet, create it and set it to disabled
+    // If it does, use its value to decide if dark-mode class must be applied or not
+    darkModeCookie === undefined ?
+        setCookie('dark-mode', 'disabled', 365) :
+        body.classList.toggle('dark-mode', darkModeCookie.value === 'enabled');
+}
 
 const fetchData = (dataKeys) => {
     return fetch('./static/data/data.json')
@@ -67,21 +96,20 @@ const displayLoader = (display) => {
 }
 
 const toggleDarkMode = () => {
-    if (localStorage.getItem('dark-mode') === 'disabled') {
+    const cookies = getCookies();
+    const darkModeCookie = cookies.find((cookie) => cookie.name.includes('dark-mode'));
+
+    if (darkModeCookie.value === 'disabled') {
         body.classList.toggle('dark-mode');
-        localStorage.setItem('dark-mode', 'enabled')
-    } else {
-        body.classList.toggle('dark-mode');
-        localStorage.setItem('dark-mode', 'disabled');
+        setCookie('dark-mode', 'enabled', 365);
     }
 
-
+    if (darkModeCookie.value === 'enabled') {
+        body.classList.toggle('dark-mode');
+        setCookie('dark-mode', 'disabled', 365);
+    }
 }
 
+setInitialCookies();
+
 darkModeBtn.addEventListener('click', toggleDarkMode);
-
-
-
-
-
-
